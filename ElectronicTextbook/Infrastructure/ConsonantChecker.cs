e3e3fad162056102a380ae0fbf26a8d5526ed20c
omega14alpha <omega14alpha@gmail.com>
2021-12-13 07:05:10 +0300

    Corrected the method 'ConsonantChecker'.

--- ElectronicTextbook/Infrastructure/ConsonantChecker.cs
+++ ElectronicTextbook/Infrastructure/ConsonantChecker.cs
@@ -4,14 +4,14 @@ namespace ElectronicTextbook.Infrastructure
 {
     internal static class ConsonantChecker
     {
+        private const string _pattern = "[бвгджзйклмнпрстфхцчшщ]";
+
         internal static bool IsStartWithConsonant(string source)
         {
             if (!string.IsNullOrWhiteSpace(source))
             {
-                string pattern = "[бвгджзйклмнпрстфхцчшщ]";
                 string input = source[0].ToString();
-
-                return Regex.IsMatch(input, pattern, RegexOptions.IgnoreCase);
+                return Regex.IsMatch(input, _pattern, RegexOptions.IgnoreCase);
             }
 
             return false;
